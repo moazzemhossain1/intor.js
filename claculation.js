e55cef051842e7a39358c02ded0e50be8c1dea59
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-15 12:49:25 +0800

    sum, sub ,multilplecion,devision,modules

---
+++ claculation.js
@@ -0,0 +1,45 @@
+// sum
+
+// const orangePrice=40;
+// const applePrice=30;
+// const tottle=orangePrice +applePrice;
+// console.log(tottle)
+
+
+//  sub
+
+
+// const orangePrice=40;
+// const applePrice=30;
+// const tottle=orangePrice - applePrice;
+// console.log(tottle)
+
+
+
+//  multipleaction
+
+
+// const orangePrice=40;
+// const applePrice=30;
+// const tottle=orangePrice * applePrice;
+// console.log(tottle)
+
+
+
+//  devesion
+
+// const orangePrice=40;
+// const applePrice=30;
+// const tottle=orangePrice / applePrice;
+// console.log(tottle)
+
+
+
+
+// mobulas
+
+
+const orangePrice=7;
+const applePrice=5;
+const tottle=orangePrice % applePrice;
+console.log(tottle)
