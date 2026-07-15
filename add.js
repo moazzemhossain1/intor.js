da45bd867755f80d26e40c0ed5ae21988daa1e18
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-15 12:30:44 +0800

    conver foolat and intejer and string

---
+++ add.js
@@ -0,0 +1,51 @@
+const orangePrice=40;
+const mongoPrice="30";
+const convartmongoPrice=parseInt(mongoPrice)
+console.log(typeof orangePrice);
+console.log(typeof convartmongoPrice);
+const tottlePrice=orangePrice+convartmongoPrice;
+console.log(typeof tottlePrice);
+console.log(tottlePrice)
+
+
+const fisrstName="Moazzem";
+const leastName="Hoassin";
+const fullName=fisrstName+leastName;
+console.log(fullName)
+
+
+// const number=40;
+// console.log(typeof number);
+// const convertFoolate=parseFloat(number);
+// console.log(typeof convertFoolate);
+// console.log(convertFoolate)
+
+
+const number=30;
+const convort=parseFloat(number);
+console.log(typeof convort);
+
+
+
+
+const fistNumber=0.12;
+const secondNumber=0.34;
+console.log(typeof fistNumber);
+console.log(typeof secondNumber);
+const tottle=fistNumber + secondNumber;
+const oneDimationa=tottle.toFixed(1)
+console.log(oneDimationa)
+
+
+
+
+
+const firstPont=0.0004;
+const secondPoint=0.00005;
+const tottlePoint=firstPont + secondPoint;
+console.log(tottlePoint);
+const threeDemational=tottlePoint.toFixed(3);
+console.log(threeDemational);
+
+const foureDemational=tottlePoint.toFixed(4);
+console.log(foureDemational)
