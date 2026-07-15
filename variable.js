c949a9630f9a6b817d8b362cfe586c55ee73e04c
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-15 08:49:18 +0800

    Variable add

---
+++ variable.js
@@ -0,0 +1,43 @@
+var age=43;
+var pepole=49;
+var name="moazzem"
+console.log(name)
+console.log(pepole)
+console.log(age)
+
+
+const banaprice=30;
+const applePrice=60;
+const goubaPrice=80;
+const totle=banaprice+applePrice+goubaPrice;
+console.log(totle);
+
+
+let price=100;
+price=50;
+price=200;
+price=170;
+price=150;
+console.log(price)
+
+
+
+
+// const appll=50;
+// const oragnge=30;
+// const subtrition=apple*oragnge;
+// console.log(subtrition)
+
+
+
+const fatherAge=80;
+const motherAge=65;
+const sub=fatherAge - motherAge;
+console.log(sub);
+
+
+
+let sisterAge=34;
+let britherAge=18;
+let mul=sisterAge * britherAge;
+console.log(mul)
