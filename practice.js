45ee246aa6ee9ca4cecf987a95e1d40479f33048
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-15 18:08:21 +0800

    All here

---
+++ practice.js
@@ -0,0 +1,82 @@
+const name1="Hello!";
+console.log(name1)
+const name2="My name is Alex.";
+console.log(name2);
+const name3="I am 20 years old.";
+console.log(name3);
+const name4="My favorite food is pizza.";
+console.log(name4);
+const name5="My favorite prgramming langauage is javaScript.";
+console.log(name5);
+const name6="I love coding:true";
+console.log(name6)
+
+
+
+// practice 01
+
+const name7="Helo JavaScript!";
+console.log(name7);
+const name8="My name is Alex.";
+console.log(name8);
+const name9="I am excited to become a web Developer.";
+console.log(name9)
+
+
+
+
+
+
+
+// pactices 02
+
+let num1=25;
+let num2=10;
+let addition=num1 +num2;
+console.log("Adition:" + addition);
+let sudtraction=num1 - num2;
+console.log("Subtraction:" + sudtraction);
+let multiplication=num1*num2;
+console.log("Multiplication:" + multiplication);
+let divison=num1 / num2;
+console.log("Division:" + divison);
+let modulus=num1 % num2;
+console.log("Modulus:" + modulus)
+
+
+
+
+let a= 100;
+let b="programming";
+let c=true;
+let d=false;
+let e=3.3323422;
+console.log("a:"+typeof a);
+console.log("b:"+typeof a);
+console.log("c:"+typeof a);
+console.log("d:"+typeof a);
+console.log("a:"+typeof a);
+console.log("a:"+typeof a);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e)
+
+
+
+
+
+let price=200;
+let quantity=3;
+let discount=10; 
+
+let totlePrice=price*quantity;
+console.log("Total Price :"+ totlePrice);
+let discountPrice=discount*quantity;
+console.log("Discount Amount :" + discountPrice);
+let finalPrice=totlePrice - discountPrice;
+console.log("Final Price:" + finalPrice)
+ 
+ 
